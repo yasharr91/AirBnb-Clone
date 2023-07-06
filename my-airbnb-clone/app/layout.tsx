4afc6fb4354f678892362adaaf9c29a59322a298
yashar amrollahi <113164350+yasharr91@users.noreply.github.com>
2023-07-06 10:24:02 +0330

    Button and modal design

--- my-airbnb-clone/app/layout.tsx
+++ my-airbnb-clone/app/layout.tsx
@@ -1,24 +1,33 @@
-import Navbar from './components/navbar/Navrbar'
-import './globals.css'
-import { Nunito } from 'next/font/google'
+"use client"
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+import Navbar from "./components/navbar/Navrbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
-        {children}</body>
+      {/* for hydration error */}
+        <ClientOnly> 
+          <Modal actionLabel="Submit" title="Modal of Mine" isOpen/>
+          <Navbar />
+        </ClientOnly>
+
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- my-airbnb-clone/app/page.tsx
+++ my-airbnb-clone/app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Home() {
   return (
     <div className="text-rose-500 text-2xl">
-        Hello Airbnb
+        {/* Hello Airbnb */}
     </div>
   );
 }
