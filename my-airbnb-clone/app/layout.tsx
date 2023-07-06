364f8e352f93372d29598ff14e634660552c6681
yashar amrollahi <113164350+yasharr91@users.noreply.github.com>
2023-07-06 22:32:32 +0330

    heading and register modal added

--- my-airbnb-clone/app/layout.tsx
+++ my-airbnb-clone/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client"
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navrbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={font.className}>
       {/* for hydration error */}
         <ClientOnly> 
-          <Modal actionLabel="Submit" title="Modal of Mine" isOpen/>
+          <RegisterModal/>
           <Navbar />
         </ClientOnly>
 
