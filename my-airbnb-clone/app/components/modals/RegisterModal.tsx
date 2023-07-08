64969695018b60c492c71febf7f598ae5314f081
yashar amrollahi <113164350+yasharr91@users.noreply.github.com>
2023-07-08 11:29:32 +0330

    next auth added, connected to mongodb

--- my-airbnb-clone/app/components/modals/RegisterModal.tsx
+++ my-airbnb-clone/app/components/modals/RegisterModal.tsx
@@ -82,10 +82,39 @@ const footerContent =(
                         <hr />
                         <Button
                         outline
-                        label='Continue with google'
+                        label='Continue with Google'
                         icon={FcGoogle}
                         onClick={()=>{}}
                         />
+                        <Button
+                        outline
+                        label='Continue with Github'
+                        icon={AiFillGithub}
+                        onClick={()=>{}}
+                        />
+                        <div
+                        className='
+                                   text-neutral-500
+                                   text-center
+                                   mt-4
+                                   font-light '>
+                                    <div className=' justify-center
+                                                    flex
+                                                    flex-row
+                                                    items-center
+                                                    gap-2'>
+                                        <div>
+                                        Already have an account?
+                                        </div>
+                                        <div onClick={registerModal.onClose}
+                                         className='
+                                                    text-neutral-800
+                                                    cursor-pointer
+                                                    hover:underline'>
+                                        Log in
+                                        </div>
+                                    </div>
+                                   </div>
     </div>
 )
     return ( <Modal
